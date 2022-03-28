21328ff0ff5b33b64cb912e5204b9365676f7112
Brunosaurio <102550094+Brunosaurio@users.noreply.github.com>
2022-03-28 19:43:11 -0300

    Add files via upload
    idk

---
+++ Untitled-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+int d;
+int main() {
+    scanf("%d",&d);
+    if(d==0){
+        printf("Hola Mundo");
+    }
+    else{
+        printf("Chau Mundo");
+    }
+  
+   return 0;
+}
